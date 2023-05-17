508df3a04731c25f5e6a5dde429c493d5a299fee
Elhaj Elhaithem <101217966+haithemelhadj@users.noreply.github.com>
2023-05-17 23:27:14 +0100

    commit

--- Assets/DrBrainsBossScript.cs
+++ Assets/DrBrainsBossScript.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrBrainsBossScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator brainsAnimator;
+    public int brainsHealth = 3;
+    public Text equation;
+
+
+
+
     void Start()
     {
-        
+        //invoke threaten in 2s
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
+    } 
+
+
+    void Charge()
+    {
+        //trigger charge animation
+        //spawn equation in canvas
+        //spawn numbers
+    }
+
+    void Hit()
+    {
+        /*
+         * if(player correct)
+         * {
+         * drbrains health--
+         * trigger hit animation
+         * invoke charge in 3s
+         * }else
+         * {
+         * pablo health--
+         * trigger threaten animation
+         * invoke charge in 3s
+         * }
+        */
+    }
+
+    void Threaten()
+    {
+        //trigger threaten animation
+        //invoke charge in 2s
     }
 }
